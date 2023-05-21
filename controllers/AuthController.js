ea0f85ffed9b530fa670d5a2ead09bd6d6bfc81d
Iago Azevedo Costa Pereira <94400482+IagoACPereira@users.noreply.github.com>
2023-05-21 15:20:04 -0300

    Add files via upload

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,3 +1,7 @@
+const User = require('../models/User')
+
+const bcrypt = require('bcryptjs')
+
 module.exports = class AuthController {
 
     static login(req, res) {
@@ -6,6 +10,19 @@ module.exports = class AuthController {
 
     static register(req, res) {
         res.render('auth/register')
+    }
+
+    static async registerPost(req, res) {
+        const { name, email, password, confirmpassword } = req.body
+
+        // Validação de senha (Se são iguais ou não)
+        if (password != confirmpassword) {
+            // Mensagem
+            req.flash('message', 'As senhas não conferem, tente novamente!')
+            res.render('auth/register')
+
+            return
+        }
     }
 
 }
